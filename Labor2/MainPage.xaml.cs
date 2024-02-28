bf5567e675c5607e60d10a68d8b8cb96cfca506f
kissbence <kissbence1515@gmail.com>
2024-02-28 11:23:17 +0200

    Initial commit

---
+++ Labor2/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Labor2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
